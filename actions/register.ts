48b0f1be54eb3619f18ce26c31830a8e0af19df1
peter michael <enitanpeters28@gmail.com>
2024-01-05 07:25:42 +0100

    prisma connected

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import * as z from "zod";
-import bcrypt from "bcryptjs";
+import bcrypt from "npx prisma generate";
 
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
