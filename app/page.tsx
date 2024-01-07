2c07cf3336eee8d53008ea108c7b0a4e92068c40
peter michael <enitanpeters28@gmail.com>
2024-01-07 10:18:21 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ const font = Poppins({
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-400 to-slate-800">
-      <div className="space-y-6 text-center">
+      <div className="space-y-6 text-center px-3">
         <h1 className={cn(
-          "text-6xl font-semibold text-white drop-shadow-md",
+          "md:text-6xl text-3xl  font-semibold text-white drop-shadow-md",
           font.className,
         )}>
            AuthJs Tutorial
