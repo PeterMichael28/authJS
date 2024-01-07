13b6fc37ca06628db38a7272e787d6d4e628d448
peter michael <enitanpeters28@gmail.com>
2024-01-07 09:42:49 +0100

    updated

--- app/(locked)/layout.tsx
+++ app/(locked)/layout.tsx
@@ -5,12 +5,12 @@ interface ProtectedLayoutProps {
 };
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
-  return ( 
-    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <Navbar />
-      {children}
-    </div>
-   );
+  return (
+   <div className="min-h-screen w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <Navbar />
+    {children}
+   </div>
+  );
 }
  
 export default ProtectedLayout;
--- lib/mail.ts
+++ lib/mail.ts
@@ -5,29 +5,29 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (
-  email: string,
-  token: string
+ email: string,
+ token: string,
 ) => {
-  await resend.emails.send({
-    from: "mail@authjs-tutorial.com",
-    to: email,
-    subject: "2FA Code",
-    html: `<p>Your 2FA code: ${token}</p>`
-  });
+ await resend.emails.send({
+  from: 'onboarding@resend.dev',
+  to: email,
+  subject: '2FA Code',
+  html: `<p>Your 2FA code: ${token}</p>`,
+ });
 };
 
 export const sendPasswordResetEmail = async (
-  email: string,
-  token: string,
+ email: string,
+ token: string,
 ) => {
-  const resetLink = `${domain}/auth/new-password?token=${token}`
+ const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
-    from: "mail@authjs-tutorial.com",
-    to: email,
-    subject: "Reset your password",
-    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
-  });
+ await resend.emails.send({
+  from: 'onboarding@resend.dev',
+  to: email,
+  subject: 'Reset your password',
+  html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
+ });
 };
 
 export const sendVerificationEmail = async (
